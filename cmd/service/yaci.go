699ed888a03c22b034e85749f80d048401f5d788
AndreaJegher <andreaj-93@hotmail.it>
2019-02-11 23:38:03 +0100

    Updated methods

--- cmd/service/yaci.go
+++ cmd/service/yaci.go
@@ -1,81 +1,101 @@
 package main
 
 import (
-  "../../yaci/chord"
-  "flag"
-  "errors"
-  "fmt"
-  "log"
-  "net"
-  "net/http"
-  "net/rpc"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+
+	"../../pkg/chord"
 )
 
 var (
-  chordServicePort = flag.Int("port", 6367, "Port of the chord service.")
-  rings = make(map[string]chord.Ring)
+	chordServicePort = flag.Int("port", 6367, "Port of the chord service.")
+	nodes            = make(map[string]chord.Node)
 )
 
 type (
-  //Service holds available chord related services
-  Service struct {
-  }
+	//Service holds available chord related services
+	Service struct {
+	}
 )
 
 //Join handles joinig a ring
 func (s Service) Join(name string, port int) error {
-  ring, err := chord.JoinRing(name, port)
-  if err != nil {
-    return err
-  }
-  rings[name] = ring
-  return nil
+	var i chord.NodeInfo
+	i.Port = port
+	ips, err := net.LookupIP(name)
+	if err != nil {
+		return err
+	}
+
+	for pos, ip := range ips {
+		i.Address = ip
+		node, err := chord.Join(i)
+		if err != nil {
+			log.Println(err)
+			if pos == len(ips)-1 {
+				return err
+			}
+		} else {
+			nodes[name] = node
+			break
+		}
+	}
+
+	return nil
 }
 
 //Create handles createing a ring
-func (s Service) Create(name string, port int) error {
-  ring, err := chord.CreateRing(name, port)
-  if err != nil {
-    return err
-  }
-  rings[name] = ring
-  return nil
+func (s Service) Create(name string, port, base int, exponent uint) error {
+	node, err := chord.Create(name, port, base, exponent)
+	if err != nil {
+		return err
+	}
+	nodes[name] = node
+	return nil
 }
 
 //Leave handles leaving a ring
 func (s Service) Leave(name string) error {
-  ring, ok := rings[name]
-  if ok {
-    return ring.Leave()
-  }
-  return errors.New("you are not in this ring")
+	node, ok := nodes[name]
+	if ok {
+		node.Running = false
+		return nil
+	}
+	return errors.New("you are not in this ring")
 }
 
 //Lookup handles lookup a key in a ring
-func (s Service) Lookup(name, key string) (net.IP, error) {
-  ring, ok := rings[name]
-  if ok {
-    return ring.LocalNode.Lookup(key)
-  }
-  return nil, errors.New("you are not in this ring")
+func (s Service) Lookup(name string, key uint64) (chord.NodeInfo, error) {
+	var i chord.NodeInfo
+	n, ok := nodes[name]
+	if ok {
+		return n.Lookup(key)
+	}
+	return i, errors.New("you are not in this ring")
 }
 
 //SimpleLookup handles lookup a key in a ring using a simple alghoritm
-func (s Service) SimpleLookup(name, key string) (net.IP, error) {
-  ring, ok := rings[name]
-  if ok {
-    return ring.LocalNode.SimpleLookup(key)
-  }
-  return nil, errors.New("you are not in this ring")
+func (s Service) SimpleLookup(name string, key uint64) (chord.NodeInfo, error) {
+	var i chord.NodeInfo
+	n, ok := nodes[name]
+	if ok {
+		return n.SimpleLookup(key)
+	}
+	return i, errors.New("you are not in this ring")
 }
 
 func main() {
-  var s Service
-  rpc.Register(s)
-  rpc.HandleHTTP()
-  l, e := net.Listen("tcp", fmt.Sprintf(":%d", *chordServicePort))
-  if e != nil {
-  	log.Fatal("listen error:", e)
-  }
-  go http.Serve(l, nil)
+	var s Service
+	rpc.Register(s)
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", fmt.Sprintf(":%d", *chordServicePort))
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	go http.Serve(l, nil)
 }
