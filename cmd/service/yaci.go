d91e4e4174b7711a11ed291ca571a9d9323889c1
AndreaJegher <andreaj-93@hotmail.it>
2019-02-12 22:33:04 +0100

    Changed create and join methods

--- cmd/service/yaci.go
+++ cmd/service/yaci.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
 	"net"
 	"net/http"
 	"net/rpc"
@@ -15,7 +16,7 @@ import (
 
 var (
 	chordServicePort = flag.Int("port", 6367, "Port of the chord service.")
-	nodes            = make(map[string]chord.Node)
+	nodes            = make(map[string]*chord.Node)
 )
 
 type (
@@ -24,47 +25,48 @@ type (
 	}
 )
 
-// Join handles joinig a ring
-func (s Service) Join(args rpchelper.ServiceArgs, reply *rpchelper.ServiceReply) error {
+// CreateRing handles createing a ring
+func (s Service) CreateRing(args rpchelper.ServiceArgs, reply *rpchelper.ServiceReply) error {
 	var i chord.NodeInfo
-	name := args.Name
-	port := args.Port
+	var r chord.RingInfo
 
-	i.Port = port
-	ips, err := net.LookupIP(name)
+	i.Port = args.Port
+	r.Name = args.Name
+	r.ModuloBase = args.Base
+	r.ModuloExponent = args.Exponent
+	r.Modulo = uint64(math.Pow(float64(r.ModuloBase), float64(r.ModuloExponent)) - 1)
+	node, err := chord.Create(i, r)
+	if err != nil {
+		return err
+	}
+	nodes[args.Name] = node
+	return nil
+}
+
+// JoinRing handles joinig a ring
+func (s Service) JoinRing(args rpchelper.ServiceArgs, reply *rpchelper.ServiceReply) error {
+	var i chord.NodeInfo
+
+	i.Port = args.Port
+	ips, err := net.LookupIP(args.Name)
 	if err != nil {
 		return err
 	}
 
 	for pos, ip := range ips {
 		i.Address = ip
-		node, err := chord.Join(i)
+		node, err := chord.Join(i, args.LocalPort)
 		if err != nil {
 			log.Println(err)
 			if pos == len(ips)-1 {
 				return err
 			}
 		} else {
-			nodes[name] = node
+			nodes[args.Name] = node
 			break
 		}
 	}
 
-	return nil
-}
-
-// Create handles createing a ring
-func (s Service) Create(args rpchelper.ServiceArgs, reply *rpchelper.ServiceReply) error {
-	name := args.Name
-	port := args.Port
-	base := args.Base
-	exponent := args.Exponent
-
-	node, err := chord.Create(name, port, base, exponent)
-	if err != nil {
-		return err
-	}
-	nodes[name] = node
 	return nil
 }
 
@@ -81,7 +83,9 @@ func (s Service) Leave(args rpchelper.ServiceArgs, reply *rpchelper.ServiceReply
 
 // List handles leaving a ring
 func (s Service) List(args rpchelper.ServiceArgs, reply *rpchelper.ServiceReply) error {
-	reply.List = nodes
+	for _, k := range nodes {
+		reply.List = append(reply.List, *k)
+	}
 	return nil
 }
 
