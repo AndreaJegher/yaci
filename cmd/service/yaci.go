57261517225990211d7228d6148e0d1115d53f63
AndreaJegher <andreaj-93@hotmail.it>
2019-02-17 15:03:35 +0100

    Fixed ring fields on creation

--- cmd/service/yaci.go
+++ cmd/service/yaci.go
@@ -33,8 +33,10 @@ func (s Service) CreateRing(args rpchelper.ServiceArgs, reply *rpchelper.Service
 	r.ModuloBase = args.Base
 	r.ModuloExponent = args.Exponent
 	r.Modulo = uint64(math.Pow(float64(r.ModuloBase), float64(r.ModuloExponent)) - 1)
+	// TODO: make the next three fields parametric
 	r.Timeout = 1e4
-	r.FingerTableDimension = 100
+	r.FingerTableLength = 100
+	r.NextBufferLength = 5
 
 	node, err := chord.Create(args.Port, r)
 	if err != nil {
